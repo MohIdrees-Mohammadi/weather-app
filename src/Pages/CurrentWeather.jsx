e22c6943ae8122a053d26258c0c0f34c54176540
Abdul Wasi <abdulwasishirzai567@gmail.com>
2026-04-19 20:59:05 +0430

    bringing icons

--- src/Pages/CurrentWeather.jsx
+++ src/Pages/CurrentWeather.jsx
@@ -4,6 +4,9 @@ import MobileCurrentWeather from '../components/MobileCurrentWeather'
 
 const CurrentWeather = () => {
   const sun = 'https://assets.api.uizard.io/api/cdn/stream/e555eccb-fbe4-4a3c-8917-933a41798140.png'
+  const semicloudy = 'https://assets.api.uizard.io/api/cdn/stream/634b1d13-e2b4-4766-9bb1-0397c173c222.png'
+  const cloudy = 'https://assets.api.uizard.io/api/cdn/stream/2c926953-f968-411a-af55-893c6c0d8901.png'
+  const rainy = 'https://assets.api.uizard.io/api/cdn/stream/d12bc406-1c84-42b5-9f37-4d52460d0521.png'
   return (
 
     <section className='text-gray-300 lg:gap-1 w-screen lg:flex '>
