edd88dceb97e40587b0706006c7bbb4c4cb60f59
Abdul Wasi <abdulwasishirzai567@gmail.com>
2026-04-19 10:13:52 +0430

    organizing searchbar

--- src/Pages/CurrentWeather.jsx
+++ src/Pages/CurrentWeather.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const CurrentWeather = () => {
+  const sun = 'https://assets.api.uizard.io/api/cdn/stream/e555eccb-fbe4-4a3c-8917-933a41798140.png'
   return (
-    <section className='text-gray-300 w-screen lg:w-full flex'>
-      <div className='lg:w-[65%]'>Left</div>
+    <main className='text-gray-300 w-screen lg:w-full flex'>
+
+      <div className='lg:w-[65%]'>
+        left
+      </div>
       <div className='lg:w-[35%]'>Right</div>
-    </section>
+    </main>
   )
 }
 
